4dd91a4234e9a178ff4ddfc502f6b141f89c5122
Dominik Jödecke <dominikjoedecke@outlook.de>
2018-09-03 09:27:19 +0200

    not register

--- src/Providers/NewsletterFS24ServiceProvider.php
+++ src/Providers/NewsletterFS24ServiceProvider.php
@@ -1,17 +1,12 @@
 <?php
 namespace NewsletterFS24\Providers;
 use Plenty\Plugin\ServiceProvider;
-/**
- * Class HelloWorldServiceProvider
- * @package HelloWorld\Providers
- */
+
+
 class NewsletterFS24ServiceProvider extends ServiceProvider
 {
-	/**
-	 * Register the service provider.
-	 */
 	public function register()
 	{
-		$this->getApplication()->register(NewsletterFS24RouteServiceProvider::class);
+		//$this->getApplication()->register(NewsletterFS24RouteServiceProvider::class);
 	}
 }
